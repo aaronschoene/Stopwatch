cc06b70d2002413927776d8bcbe6601e3b80a73a
Yotam Ohayon <ohayon.yotam@gmail.com>
2019-06-07 14:04:17 +0200

    started empty project

---
+++ Stopwatch/Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by yohayon on 07.06.19.
+//  Copyright © 2019 Yotam Ohayon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
