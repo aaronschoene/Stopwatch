5fa32df963e3b6262968b77f7e8d5c7d8ca81725
Aaron Schoene <aarom.schoeneme.com>
2019-06-07 15:11:45 +0200

    Added start and stop funcionallity

--- Stopwatch/Stopwatch/ViewController.swift
+++ Stopwatch/Stopwatch/ViewController.swift
@@ -12,12 +12,18 @@ class ViewController: UIViewController {
     
     let label = UILabel()
     let startButton = UIButton()
+    let stopButton = UIButton()
+    
+    var timer: Timer?
+    
+    var numerofseconds = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         addLabel()
         addStartButton()
+        addStopButton()
     }
 
     func addLabel() {
@@ -27,7 +33,7 @@ class ViewController: UIViewController {
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100.0)
             ])
-        label.text = "00:00"
+        label.text = "\(numerofseconds)"
     }
     
     func addStartButton() {
@@ -40,13 +46,51 @@ class ViewController: UIViewController {
             ])
         startButton.setTitle("Start", for: .normal)
         startButton.setTitleColor(.blue, for: .normal)
+        startButton.setTitleColor(.gray, for: .disabled)
         startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
     }
     
     @objc func startButtonTapped() {
         print("start button tapped")
-        label.text = "00:01"
+        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updatelabel), userInfo: nil, repeats: true)
+        startButton.isEnabled = false
+        stopButton.setTitle("Stop", for: .normal)
     }
-
+    
+    @objc func updatelabel() {
+        numerofseconds = 1 + numerofseconds
+        label.text = "\(numerofseconds)"
+    }
+    
+    func addStopButton() {
+        stopButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stopButton)
+        NSLayoutConstraint.activate([
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.centerYAnchor.constraint(equalTo: label.centerYAnchor,
+                                                 constant: 140.0)
+            ])
+        stopButton.setTitle("Stop", for: .normal)
+        stopButton.setTitleColor(.red, for: .normal)
+        stopButton.addTarget(self, action: #selector(stopButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func stopButtonTapped() {
+        print("stop button tapped")
+        if stopButton.title(for: .normal) == "Stop" {
+            self.timer?.invalidate()
+            startButton.isEnabled = true
+            startButton.setTitle("Continue", for: .normal)
+            stopButton.setTitle("Reset", for: .normal)
+        }
+        
+        else if stopButton.title(for: .normal) == "Reset" {
+            self.timer?.invalidate()
+            numerofseconds = 0
+            label.text = "\(numerofseconds)"
+            startButton.setTitle("Start", for: .normal)
+            stopButton.setTitle("Stop", for: .normal)
+             startButton.isEnabled = true
+        }
+    }
 }
-
